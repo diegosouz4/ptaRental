604a606914412d0052083dd1d9f67617e1c1e49a
Souza <disouzauva@gmail.com>
2024-07-08 23:53:06 -0300

    feat: section cursos

--- src/js/modules/Slider-home.js
+++ src/js/modules/Slider-home.js
@@ -1,4 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
+  slideHero();
+  slideSoluctions();
+});
+
+function slideHero() {
   const slider = {
     wrapper: document.querySelector("[data-homeSlider='wrapper']"),
     itens: document.querySelectorAll("[data-homeSlider='slide']"),
@@ -11,8 +16,48 @@ document.addEventListener("DOMContentLoaded", () => {
     infinite: false,
     slidesToShow: 1,
     slidesToScroll: 1,
-    lazyLoad: 'ondemand',
+    lazyLoad: "ondemand",
     autoplay: true,
     autoplaySpeed: 5000,
   });
-});
+}
+
+function slideSoluctions() {
+  const slider = {
+    wrapper: document.querySelector("[data-coursesSlider='wrapper']"),
+    itens: document.querySelectorAll("[data-coursesSlider='slide']"),
+  };
+
+  if (!slider.wrapper || slider.itens.length < 2) return;
+
+  $("[data-coursesSlider='wrapper']").slick({
+    dots: false,
+    infinite: false,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    lazyLoad: "ondemand",
+    responsive: [
+      {
+        breakpoint: 900,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        }
+      },
+      {
+        breakpoint: 500,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        }
+      },
+    ]
+  });
+}
